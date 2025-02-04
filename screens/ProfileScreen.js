6e0556f743f7aa1137f197bda005d9575a6d6fd5
Maximilian <maximilianpeh2024@gmail.com>
2025-02-04 00:41:24 -0500

    basic profile + userData

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,9 +1,13 @@
 import { View, Text, StyleSheet } from 'react-native';
+import { userData } from '../data/userData';
 
 export default function ProfileScreen() {
   return (
     <View style={styles.container}>
-      <Text>Profile Screen</Text>
+      <Text>username: {userData.username}</Text>
+      <Text>rating: {userData.rating}</Text>
+      <Text>followers: {userData.followers}</Text>
+      <Text>following: {userData.following}</Text>
     </View>
   );
 }
