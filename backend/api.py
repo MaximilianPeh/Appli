ea71d10cf607f2d5980526b61d563da2a640b171
al1cezhan9 <alice.az.zhang@gmail.com>
2025-02-08 19:27:10 -0500

    Add firebase configuration

--- backend/api.py
+++ backend/api.py
@@ -1,17 +1,45 @@
 from flask import Flask, request, jsonify
 from recommendations import get_recommendations
 from autotagging import get_autotag
+from recommendations import get_recommendations
+from autotagging import get_autotag
 
 app = Flask(__name__)
+
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+cred = credentials.Certificate("appli-b7af4-firebase-adminsdk-fbsvc-56b516817b.json")
+firebase_admin.initialize_app(cred)
+
+
+db = firestore.client()
+
 
 
 @app.route('/recommendations', methods=['POST']) # post means send data to api; get means you get data from firebase
 def recommendations():
     data = request.json
-    history = data.get('user_history')
-    recommendations = get_recommendations(history)
+    user_id = data.get('user_id')
+    recommendations = get_recommendations(user_id)
     return jsonify(recommendations)
 
+@app.route('/recommendations', methods=['POST']) # post means send data to api; get means you get data from firebase
+def recommendations():
+    data = request.json
+    user_id = data.get('user_id')
+    recommendations = get_recommendations(user_id)
+    return jsonify(recommendations)
+
+
+@app.route('/autotag', methods=['POST'])
+def autotag():
+    data = request.json
+    image_path = data.get('image_path')
+    autotag = get_autotag(image_path)
+    return jsonify(autotag)
 
 @app.route('/autotag', methods=['POST'])
 def autotag():
